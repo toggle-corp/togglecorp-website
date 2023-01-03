95450cae558b892e25795c535c05caa33c3c89e0
barshathakuri <barshathakuri708@gmail.com>
2023-01-03 15:54:36 +0545

    Add images to works

--- src/data/projectImages.ts
+++ src/data/projectImages.ts
@@ -1,10 +1,13 @@
 import workListOne from 'resources/work-list-1.webp';
 import ifrcGo from 'resources/ifrc-go-home.webp';
-import durableSolution from 'resources/durable-solutions-nepal.webp';
+import durableSolution from 'resources/durable-solutions.webp';
 import kitabBazar from 'resources/kitab-bazar-home.webp';
 import deepHome from 'resources/deep-home.webp';
 import idmcGIDD from 'resources/global-internal-displacement-database.webp';
-
+import sikkaImg from 'resources/sikkaImg.webp';
+import npStocks from 'resources/np-stocks.webp';
+import dfidInfographics from 'resources/dfid-infographics.webp';
+import dfidImg from 'resources/dfid-dashboard.webp';
 import { ProjectId } from 'data/projects';
 
 // NOTE: we need to separate out images because they cannot be passed through getStaticProps
@@ -12,11 +15,11 @@ const projectCoverImages: {
     [key in ProjectId]?: string;
 } = {
     'ifrc-go': ifrcGo,
-    dfid: workListOne,
-    npstocks: workListOne,
+    dfid: dfidImg,
+    npstocks: npStocks,
     'innovation-project': workListOne,
     'durable-solutions': durableSolution,
-    'dfid-infographics': workListOne,
+    'dfid-infographics': dfidInfographics,
     'management-information-system': workListOne,
     sustain: workListOne,
     'kitab-bazar': kitabBazar,
@@ -25,7 +28,7 @@ const projectCoverImages: {
     'idmc-gidd': idmcGIDD,
     // 'child-sponser': workListOne,
     helix: workListOne,
-    sikka: workListOne,
+    sikka: sikkaImg,
     paalika: workListOne,
 };
 
