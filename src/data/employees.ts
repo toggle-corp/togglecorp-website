50bcf858e45aa6457e453265badae0c15f7f6769
barshathakuri <barshathakuri708@gmail.com>
2023-01-13 16:37:32 +0545

    Add some employee pictures, description and add link to employee card

--- src/data/employees.ts
+++ src/data/employees.ts
@@ -36,9 +36,9 @@ export type EmployeeId = 'aditya-khatri'
 export interface Employee {
     id: EmployeeId;
     name: string;
-    description: string;
+    funnyDescription?: string;
+    description?: string;
     position: string;
-    joinedAt: string;
     department: Department;
 
     // TODO: add more links
@@ -50,17 +50,18 @@ const adityaKhatri: Employee = {
     id: 'aditya-khatri',
     name: 'Aditya Khatri',
     position: 'Co-founder / Developer',
-    description: 'Dherai halla, thorai kaam',
-    joinedAt: '2016-11-18',
+    funnyDescription: 'Dherai halla, thorai kaam',
     department: 'development',
+    description: '"Aditya here! Come talk to me, or rather listen to be explain ideas and facts that you probably don\'t need to know. I like music, I like cooking and love food. I am constantly changing hobbies and trying out new things. I think everyone should appreciate effort more that the outcome. If we live everyday with an effort to be better than yesterday, the least we\'ll be is what we are right now, which is okay."',
+    linkedInLink: 'https://www.linkedin.com/in/adityakhatri47/',
+    instagramLink: 'https://www.instagram.com/adityakhatri47/',
 };
 
 const amarShahi: Employee = {
     id: 'amar-shahi',
     name: 'Amar Shahi',
     position: 'Operations Manager',
-    description: 'Experienced Project Manager with a demonstrated history of working in the information technology and services industry with a Master of Business Administration (M.B.A.) focused in Project Management from Apex College.',
-    joinedAt: '2010-10-12',
+    funnyDescription: 'Experienced Project Manager with a demonstrated history of working in the information technology and services industry with a Master of Business Administration (M.B.A.) focused in Project Management from Apex College.',
     department: 'operations',
 };
 
@@ -68,17 +69,17 @@ const ankitMehta: Employee = {
     id: 'ankit-mehta',
     name: 'Ankit Mehta',
     position: 'CEO / Co-founder',
-    description: 'I\'m the CEO, plich',
-    joinedAt: '2010-10-12',
+    funnyDescription: 'I\'m the CEO, plich',
+    description: 'Let\'s keep this as a placeholder for now',
     department: 'development',
+    linkedInLink: 'https://www.linkedin.com/in/frozenhelium/',
 };
 
 const anushaKhadka: Employee = {
     id: 'anusha-khadka',
     name: 'Anusha Khadka',
     position: 'Analyst',
-    description: 'Baisakh mahina ma ful fulyo, timlai dekhera man bhulyo',
-    joinedAt: '2010-10-12',
+    funnyDescription: 'Baisakh mahina ma ful fulyo, timlai dekhera man bhulyo',
     department: 'analysis',
 };
 
@@ -86,8 +87,8 @@ const barshaThakuri: Employee = {
     id: 'barsha-thakuri',
     name: 'Barsha Thakuri',
     position: 'Developer',
-    description: 'I\'ll be late for standup, My scotty is not starting',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'I\'ll be late for standup, My scotty is not starting',
+    description: 'Barsha loves adventures, traveling, hiking, visiting new places, and reading books. "Mountains are her happy place"',
     department: 'development',
     linkedInLink: 'https://www.linkedin.com/in/barshathakuri/',
     instagramLink: 'https://www.instagram.com/barsha.thakuri/',
@@ -97,8 +98,8 @@ const bibekPandey: Employee = {
     id: 'bibek-pandey',
     name: 'Bibek Pandy',
     position: 'Developer',
-    description: '50 katechi ramaula!',
-    joinedAt: '2012-10-12',
+    funnyDescription: '50 katechi ramaula!',
+    description: '"Chitwan ma ghar chha, Hamlai ke ko darr chha, Dui din ko jindagi maa, hasi raakhnu parcha." Computer and Mathematics. Traveling and writing.',
     department: 'development',
 };
 
@@ -106,8 +107,7 @@ const bimalPandey: Employee = {
     id: 'bimal-pandey',
     name: 'Bimal Pandey',
     position: 'Developer',
-    description: 'Namaskar sir haru!',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'Namaskar sir haru!',
     department: 'development',
 };
 
@@ -115,8 +115,7 @@ const keyurKhadka: Employee = {
     id: 'keyur-khadka',
     name: 'Keyur Khadka',
     position: 'Developer',
-    description: 'Stay hard! (PS: Context is important!)',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'Stay hard! (PS: Context is important!)',
     department: 'development',
 };
 
@@ -124,8 +123,7 @@ const priyeshGautam: Employee = {
     id: 'priyesh-gautam',
     name: 'Priyesh Gautam',
     position: 'Developer',
-    description: 'Get yo self some juicy ass jerris!',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'Get yo self some juicy ass jerris!',
     department: 'development',
 };
 
@@ -133,8 +131,7 @@ const subinaRajopadhyay: Employee = {
     id: 'subina-rajopadhyaya',
     name: 'Subina Sharma Rajopadhyaya',
     position: 'Developer',
-    description: 'Subina ta Su bina Bina hudo raichha',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'Subina ta Su bina Bina hudo raichha',
     department: 'development',
 };
 
@@ -142,8 +139,7 @@ const smritiKafle: Employee = {
     id: 'smriti-kafle',
     name: 'Smriti Kafle',
     position: 'Designer',
-    description: 'Kadak! Clk! Clk!',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'Kadak! Clk! Clk!',
     department: 'development',
 };
 
@@ -151,8 +147,7 @@ const shubhDhital: Employee = {
     id: 'shubh-dhital',
     name: 'Shubh Dhital',
     position: 'QA Engineer',
-    description: 'Yo description padhirako Fiiiiixxxx ho!',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'Yo Description padhirako Fiiiiixxxx ho!',
     department: 'development',
 };
 
@@ -160,8 +155,7 @@ const safarLigal: Employee = {
     id: 'safar-ligal',
     name: 'Safar Sanu Ligal',
     position: 'Developer',
-    description: 'Mazza ayena, eh!',
-    joinedAt: '2012-10-12',
+    funnyDescription: 'Mazza ayena, eh!',
     department: 'development',
 };
 
@@ -169,8 +163,7 @@ const kriti: Employee = {
     id: 'kriti-chhetri',
     name: 'Kriti Chhetri',
     position: 'Analyst',
-    description: 'Chyaaaaaaannn! Out! Out!',
-    joinedAt: '2011-10-12',
+    funnyDescription: 'Chyaaaaaaannn! Out! Out!',
     department: 'analysis',
 };
 
