bf2e48da785c7780ba8c941e6759f6b92aa3a327
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2023-01-23 12:35:02 +0545

    Add employee images

--- src/data/employeeImages.ts
+++ src/data/employeeImages.ts
@@ -10,28 +10,28 @@ import bimalPandey from 'resources/employee/bimal-pandey.png';
 // import karinaGopali from 'resources/employee/karina-gopali.png';
 import keyurKhadka from 'resources/employee/keyur-khadka.png';
 // import kisanPahari from 'resources/employee/kisan-pahari.png';
-// import kritiChhetri from 'resources/employee/kriti-chhetri.png';
-// import navinAyer from 'resources/employee/navin-ayer.png';
+import kritiChhetri from 'resources/employee/kriti-chhetri.png';
+import navinAyer from 'resources/employee/navin.png';
 // import nikaShakya from 'resources/employee/nika-shakya.png';
 // import poojaSharma from 'resources/employee/pooja-sharma.png';
 // import prinishaBaidhya from 'resources/employee/prinisha-baidhya.png';
 import priyeshGautam from 'resources/employee/priyesh-gautam.png';
 // import puranBan from 'resources/employee/puran-ban.png';
-// import ranjanShrestha from 'resources/employee/ranjan-shrestha.png';
+import ranjanShrestha from 'resources/employee/ranjan.png';
 // import rishiKhatri from 'resources/employee/rishi-khatri.png';
 // import rubyThapaliya from 'resources/employee/ruby-thapaliya.png';
 import safarLigal from 'resources/employee/safar-ligal.png';
-// import sagunaThapa from 'resources/employee/saguna-thapa.png';
+import sagunaThapa from 'resources/employee/saguna.png';
 // import sameerRai from 'resources/employee/sameer-rai.png';
-// import shreetiKafle from 'resources/employee/shreeti-kafle.png';
+import shreetiKafle from 'resources/employee/shreeti-kafle.png';
 // import shreeyaShrestha from 'resources/employee/shreeya-shrestha.png';
 // import shreeyashShrestha from 'resources/employee/shreeyash-shrestha.png';
 // import shristiManandhar from 'resources/employee/shristi-manandhar.png';
 import shubhDhital from 'resources/employee/shubh-dhital.png';
 import smritiKafle from 'resources/employee/smriti-kafle.png';
-// import snehaPandey from 'resources/employee/sneha-pandey.png';
+import snehaPandey from 'resources/employee/sneha-pandey.png';
 import subinaRajopadhyaya from 'resources/employee/subina-rajopadhyaya.png';
-// import udayMaharjan from 'resources/employee/uday-maharjan.png';
+import udayMaharjan from 'resources/employee/uday-maharjan.png';
 
 import { EmployeeId } from 'data/employees';
 
@@ -44,7 +44,6 @@ const employeeImages: {
     'anusha-khadka': anushaKhadka,
     'barsha-thakuri': barshaThakuri,
     // 'bibek-pandey': bibekPandey,
-    'kriti-chhetri': employeeIcon,
     'safar-ligal': safarLigal,
     'subina-rajopadhyaya': subinaRajopadhyaya,
     'keyur-khadka': keyurKhadka,
@@ -53,6 +52,12 @@ const employeeImages: {
     'priyesh-gautam': priyeshGautam,
     'smriti-kafle': smritiKafle,
     'shubh-dhital': shubhDhital,
+    'kriti-chhetri': kritiChhetri,
+    'navin-ayer': navinAyer,
+    'shreeti-kafle': shreetiKafle,
+    'sneha-pandey': snehaPandey,
+    'uday-maharjan': udayMaharjan,
+    'saguna-thapa': sagunaThapa,
 };
 
 // eslint-disable-next-line import/prefer-default-export
