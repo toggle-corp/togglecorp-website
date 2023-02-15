71f83f151e03cc4fec13c5f2903bb1b5212349ed
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2023-02-15 09:42:05 +0545

    PR fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -192,7 +192,7 @@ function Home() {
                 <div className={styles.projectCount}>
                     <div className={styles.statBox}>
                         <div className={styles.statBoxContainer}>
-                            <IoIosFiling size={32} color="white" />
+                            <IoIosFiling size={32} className={styles.statIcon} />
                         </div>
                         <div className={styles.statInfo}>
                             <span className={styles.statNumber}>
@@ -205,7 +205,7 @@ function Home() {
                     </div>
                     <div className={styles.statBox}>
                         <div className={styles.statBoxContainer}>
-                            <IoIosRibbon size={32} color="white" />
+                            <IoIosRibbon size={32} className={styles.statIcon} />
                         </div>
                         <div className={styles.statInfo}>
                             <span className={styles.statNumber}>
@@ -218,7 +218,7 @@ function Home() {
                     </div>
                     <div className={styles.statBox}>
                         <div className={styles.statBoxContainer}>
-                            <IoIosPeople size={32} color="white" />
+                            <IoIosPeople size={32} className={styles.statIcon} />
                         </div>
                         <div className={styles.statInfo}>
                             <span className={styles.statNumber}>
